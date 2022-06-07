32f03a5545dc7f436f9de7dcbbe7752d06562a8e
aviram5 <60235058+aviram5@users.noreply.github.com>
2022-06-07 14:01:43 +0300

    install packages

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {Text, View} from 'react-native';
+import * as eva from '@eva-design/eva';
+import AppNavigator from './src/navigation/AppNavigator';
+import {store} from './src/Store';
+import {Provider as StoreProvider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 
-const App = () => {
+export default function App() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>App</Text>
-    </View>
+    <StoreProvider store={store}>
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <IconRegistry icons={EvaIconsPack} />
+        <NavigationContainer>
+          <IconRegistry icons={EvaIconsPack} />
+          <ApplicationProvider {...eva} theme={eva['light']}>
+            <AppNavigator />
+          </ApplicationProvider>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </StoreProvider>
   );
-};
-
-export default App;
+}
