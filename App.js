f2256ffe8bd218a682fec3043d04203dd064c084
aviram5 <60235058+aviram5@users.noreply.github.com>
2022-06-08 15:58:28 +0300

    app

--- App.js
+++ App.js
@@ -4,25 +4,16 @@ import AppNavigator from './src/navigation/AppNavigator';
 import {store} from './src/Store';
 import {Provider as StoreProvider} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
-import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
-import {
-  SafeAreaProvider,
-  initialWindowMetrics,
-} from 'react-native-safe-area-context';
-import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import {ApplicationProvider} from '@ui-kitten/components';
 
 export default function App() {
   return (
     <StoreProvider store={store}>
-      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <IconRegistry icons={EvaIconsPack} />
-        <NavigationContainer>
-          <IconRegistry icons={EvaIconsPack} />
-          <ApplicationProvider {...eva} theme={eva['light']}>
-            <AppNavigator />
-          </ApplicationProvider>
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <ApplicationProvider {...eva} theme={eva['light']}>
+          <AppNavigator />
+        </ApplicationProvider>
+      </NavigationContainer>
     </StoreProvider>
   );
 }
