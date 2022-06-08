288502ce9cf81436a4c8d7a67a5d8061c00d2759
aviram5 <60235058+aviram5@users.noreply.github.com>
2022-06-08 15:58:43 +0300

    Update FakeServer.js

--- src/api/FakeServer.js
+++ src/api/FakeServer.js
@@ -2,7 +2,7 @@ export const FakeServerRequest = formData =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       let p = Math.random() * 100;
-      if (p < 10) {
+      if (p < 80) {
         resolve(formData);
       } else {
         reject('An error occurred please save again');
