6965a7025175d4eef1a605811e20c35c563527c7
aviram5 <60235058+aviram5@users.noreply.github.com>
2022-06-08 15:56:11 +0300

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,15 +1,31 @@
 import React from 'react';
-// import {Header} from '~layout/Header';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import tabsConfig from '../config/tabConfig';
+import {Text} from '@ui-kitten/components';
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
-    <Tab.Navigator initialRouteName="form">
+    <Tab.Navigator
+      initialRouteName="form"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#4293d7',
+        tabBarInactiveTintColor: '#000',
+        tabBarShowLabel: false,
+      }}>
       {tabsConfig.map((tab, index) => {
         return (
-          <Tab.Screen key={index} name={tab.title} component={tab.component} />
+          <Tab.Screen
+            key={index}
+            name={tab.title}
+            component={tab.component}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Text style={{color, fontSize: 24}}>{tab.title}</Text>
+              ),
+            }}
+          />
         );
       })}
     </Tab.Navigator>
