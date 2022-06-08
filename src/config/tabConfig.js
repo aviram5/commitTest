a8cf1136d048e2a991072b0d5df5c0eb62f13bbe
aviram5 <60235058+aviram5@users.noreply.github.com>
2022-06-08 15:58:55 +0300

    Update tabConfig.js

--- src/config/tabConfig.js
+++ src/config/tabConfig.js
@@ -3,12 +3,10 @@ import {Form, User} from '../screens';
 const tabsConfig = [
   {
     title: 'Form',
-    // icon: 'shopping-cart',
     component: Form,
   },
   {
     title: 'user',
-    // icon: 'plus-square',
     component: User,
   },
 ];
